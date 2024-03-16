496dec797eda871249dc12805666f4ece08d2d5e
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-16 14:36:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
-    //Write your code here
-
+  // Get form values
+  let fname = document.getElementsByName('fname')[0].value;
+  let lname = document.getElementsByName('lname')[0].value;
+  
+  // Display alert with form values
+  window.alert(`${fname} ${lname}`);
 }
